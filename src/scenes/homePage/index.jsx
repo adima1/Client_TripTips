3c2d8197409dea955c8b12ae0f69a6e4269703fc
adi <adistudent45@gmail.com>
2024-07-21 02:42:30 +0300

    all2

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
+const HomePage = ()=> {
+    return (<Box>
+        <Navbar />
+        </Box>);
+};
+
+export default HomePage;
+
+/*const HomePage = ()=> {
+
+    return (<div>HomePage</div>)
+}
+
+export default HomePage;*/
